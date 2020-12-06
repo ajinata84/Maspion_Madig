2cc1b904df8c0e8dbaedf3659cd8fca03e25a35c
lleans <ahmadunnail@gmail.com>
2020-12-06 18:56:45 +0700

    remove services dart at home.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:madicc/theme.dart';
 import 'package:provider/provider.dart';
 import 'pages/pages.dart';
-import 'package:flutter/services.dart';
 
 void main() {
   runApp(MyApp());
