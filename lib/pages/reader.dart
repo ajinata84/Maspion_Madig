f0766fc8bb2803f88ddde6ae389c40a8b84abdee
ajinata84 <ajinata89@gmail.com>
2020-12-06 10:45:52 +0700

    moved article widgets to its own class

--- lib/pages/reader.dart
+++ lib/pages/reader.dart
@@ -1,6 +1,7 @@
 import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:madicc/models/articlemodel.dart';
+import 'package:madicc/widgets/articlewidgets.dart';
 import 'package:provider/provider.dart';
 
 import '../data.dart';
@@ -35,62 +36,15 @@ class _ReaderState extends State<Reader> {
             padding: const EdgeInsets.symmetric(vertical: 15.0),
             child: Column(
               children: [
-                buildFullImage('assets/images/dummy/0.jpg'),
-                buildText('Maspion merupakan salah satu produk yang cukup legendaris di Indonesia. Orang-orang Indonesia akan selalu teringat dengan iklan seorang laki-laki blasteran Tionghoa-Indonesia, bernama Alim Markus, bersama artis Titiek Puspa dengan jargonnya: “Cintailah produk-produknya Indonesia.” Tak hanya Titie Puspa saja yang diajak menjadi bintang iklan Maspion, menurut buku 60 Tahun Jimly Asshiddiqie: Sosok, Kiprah dan Pemikiran (2016), mantan Ketua Mahkamah Konstitusi Jimly Asshiddiqie juga pernah ditawari membintangi iklan tersebut. Karena role modelnya adalah Titiek Puspa, Jimly pun tak keberatan untuk bilang: “Cintailah produk-produknya Indonesia.”'),
-                buildTextHighlight('Tidak ada yang salah dengan ucapan itu, karena memang produk-produk Maspion diproduksi di Indonesia. Sejarah Maspion, boleh dibilang termasuk juga sejarah industri Indonesia. Seperti juga sejarah industri milik Gobel Panasonic, meski kalah tua.')
+                buildFullImage('assets/images/dummy/0.jpg',context),
+                buildText('Maspion merupakan salah satu produk yang cukup legendaris di Indonesia. Orang-orang Indonesia akan selalu teringat dengan iklan seorang laki-laki blasteran Tionghoa-Indonesia, bernama Alim Markus, bersama artis Titiek Puspa dengan jargonnya: “Cintailah produk-produknya Indonesia.” Tak hanya Titie Puspa saja yang diajak menjadi bintang iklan Maspion, menurut buku 60 Tahun Jimly Asshiddiqie: Sosok, Kiprah dan Pemikiran (2016), mantan Ketua Mahkamah Konstitusi Jimly Asshiddiqie juga pernah ditawari membintangi iklan tersebut. Karena role modelnya adalah Titiek Puspa, Jimly pun tak keberatan untuk bilang: “Cintailah produk-produknya Indonesia.”',context),
+                buildTextHighlight('Tidak ada yang salah dengan ucapan itu, karena memang produk-produk Maspion diproduksi di Indonesia. Sejarah Maspion, boleh dibilang termasuk juga sejarah industri Indonesia. Seperti juga sejarah industri milik Gobel Panasonic, meski kalah tua.',context)
               ],
             ),
           ),
         )
       ]),
     ));
-  }
-
-  Padding buildTextHighlight(String text) {
-    return Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                child: SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.89,
-                    child: Text(
-                      '           $text',
-                      style: TextStyle(
-                          fontSize: 12,
-                          fontFamily: 'Moserat',
-                          fontWeight: FontWeight.w900,
-                          color: Color(0xFFEC1B34)),
-                      textAlign: TextAlign.justify,
-                    )),
-              );
-  }
-
-  Widget buildFullImage(String imgPath) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8.0),
-      child: Container(
-        height: 200,
-        width: MediaQuery.of(context).size.width * 0.89,
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(15),
-            image:
-                DecorationImage(image: AssetImage(imgPath), fit: BoxFit.cover)),
-      ),
-    );
-  }
-
-  Widget buildText(String text) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8.0),
-      child: SizedBox(
-          width: MediaQuery.of(context).size.width * 0.89,
-          child: Text(
-            '              $text',
-            style: TextStyle(
-                fontSize: 12,
-                fontFamily: 'Moserat',
-                fontWeight: FontWeight.w600),
-            textAlign: TextAlign.justify,
-          )),
-    );
   }
 
   Widget appBar(BuildContext context) {
